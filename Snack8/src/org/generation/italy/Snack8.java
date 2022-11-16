2a3509f0c4afd9fe3c5dec484c34f0b23b373597
ValerioGc <valerio.gc1@gmail.com>
2022-11-16 16:37:13 +0100

    avanzamento snack8

--- Snack8/src/org/generation/italy/Snack8.java
+++ Snack8/src/org/generation/italy/Snack8.java
@@ -10,13 +10,15 @@ public class Snack8 {
 		
 		int maxValue = 0;
 		int minValue = 10;
-		
+		int sumMFive = 0;
 		
 		for (int i = 0; i < numbers.length; i++) {
-			numbers[i] = rnd.nextInt(50) - 50; 
+			numbers[i] = rnd.nextInt(100) - 50; 
 			System.out.println(numbers[i]);
+			if ((numbers[i] % 5) == 0) {
+				sumMFive += (int)numbers[i];
+			}
 		}
-		
-		
+		System.out.println("somma numeri:" + sumMFive);
 	}
 }
