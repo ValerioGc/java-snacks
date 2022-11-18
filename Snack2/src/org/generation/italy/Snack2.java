db4086f61ea103476560c44b07567ca4c5cdba19
ValerioGc <valerio.gc1@gmail.com>
2022-11-18 15:44:52 +0100

    Rev Snack 2

--- Snack2/src/org/generation/italy/Snack2.java
+++ Snack2/src/org/generation/italy/Snack2.java
@@ -1,5 +1,16 @@
 package org.generation.italy;
 
-public class Snack2 {
+import java.util.Random;
 
+public class Snack2 {
+	public static void main(String[] args) {
+		String[] randNames = {"Luca", "Alessio", "Simone", "Valerio", "Mario"};
+		String[] randSurnames = {"Rossi", "Bianchi", "Verdi", "Proietti", "cognome"};
+		Random rnd = new Random();
+		int numberC = rnd.nextInt(5);		
+		int numberC2 = rnd.nextInt(5);
+			System.out.println("Nome invitato: " + randNames[numberC] + " " + randSurnames[numberC2]);
+		
+		
+	}
 }
